8f4d30f06d9df6df4066abed5e7c562711d7f388
Rahul <caulirahul6@gmail.com>
2025-06-06 16:56:45 +0530

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <h1 className="text-4xl font-bold text-blue-600">
+      Hello Tailwind + React + Vite!
+    </h1>
+  );
+}
+
+export default App;
